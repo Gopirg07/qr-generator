dc2f89460c2080dbedc9e2d84e8cfa087f0ab645
Gopirg07 <gopinathofficial16@gmail.com>
2023-09-10 13:47:26 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
-import QRCode from "react-qr-code";
+// import QRCode from "react-qr-code";
+import QRCode from "qrcode";
 import {
   Button,
   FormControl,
@@ -9,52 +10,88 @@ import {
   Select,
   TextField,
 } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [age, setAge] = React.useState("");
+  const [value, setValue] = useState("");
+  const [height, setHeight] = useState("250");
+  const [width, setWidth] = useState("250");
+  const [qr, setQr] = useState("#000000");
+  const [bg, setBg] = useState("#ffffff");
+  const [jpg, setJpg] = useState(".jpg");
+  const [png, setPng] = useState(".png");
+  const [svg, setSvg] = useState(".svg");
 
+  const [imgu, setImgu] = useState("");
+
+  const [age, setAge] = React.useState("");
   const handleChange = (event) => {
     setAge(event.target.value);
   };
+  async function generateQrCode() {
+    try { 
+      const res = await QRCode.toDataURL(value);
+      setImgu(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div className="App container">
       <h1>Qr Code Generator</h1>
-      <div style={{alignItems:"center"}} className="row">
+      <div style={{ alignItems: "center" }} className="row">
         <div className="col-md-6 details-div">
-          <p style={{textAlign:"start"}}>Enter The Data</p>
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+          <p style={{ textAlign: "start" }}>Data</p>
+          <TextField
+            id="outlined-basic"
+            label="Enter The Data"
+            variant="outlined"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
 
           <div className="h-w-div">
             <div>
-              <p>Height</p>
+              <p style={{ textAlign: "start" }}>Height</p>
               <TextField
                 id="outlined-basic"
-                label="Outlined"
+                label="Enter height"
                 variant="outlined"
+                value={height}
+                onChange={(e) => setHeight(e.target.value)}
               />
             </div>
 
             <div>
-              <p>width</p>
+              <p style={{ textAlign: "start" }}>Width</p>
               <TextField
                 id="outlined-basic"
-                label="Outlined"
+                label="Enter width"
                 variant="outlined"
+                value={width}
+                onChange={(e) => setWidth(e.target.value)}
               />
             </div>
           </div>
-          <div className="qrc-div">
+          {/* <div className="qrc-div">
             <p>QR Color : </p>
-            <input type="color" />
-          </div>
+            <input
+              type="color"
+              value={qr}
+              onChange={(e) => setQr(e.target.value)}
+            />
+          </div> 
           <div className="qrc-div">
             <p>Background Color : </p>
-            <input type="color" />
+            <input
+              type="color"
+              value={bg}
+              onChange={(e) => setBg(e.target.value)}
+            />
           </div>
           <div className="format-select">
             <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Age</InputLabel>
+              <InputLabel id="demo-simple-select-label">Format</InputLabel>
               <Select
                 labelId="demo-simple-select-label"
                 id="demo-simple-select"
@@ -62,21 +99,36 @@ function App() {
                 label="Age"
                 onChange={handleChange}
               >
-                <MenuItem value={10}>Ten</MenuItem>
-                <MenuItem value={20}>Twenty</MenuItem>
-                <MenuItem value={30}>Thirty</MenuItem>
+                <MenuItem value={jpg}>jpg</MenuItem>
+                <MenuItem value={png}>png</MenuItem>
+                <MenuItem value={svg}>svg</MenuItem>
               </Select>
             </FormControl>
-          </div>
+          </div> */}
           <div className="btnss">
-            <Button color="success" variant="contained">
+            <Button
+              color="success"
+              variant="contained"
+              onClick={() => generateQrCode()}
+            >
               Generate
             </Button>
-            <Button variant="contained">Download</Button>
+            <Button variant="contained"><a href={imgu} download style={{color:"white",textDecoration:"none"}}>Download</a></Button>
           </div>
         </div>
         <div className="qr-main col-md-6">
-          <QRCode style={{height:"400px",width:"400px"}} value="Gopinath" />
+          <img
+            // style={{ height: `350px`, width: `350px` }}
+            style={{ height: `${height}px`, width: `${width}px` }}
+            src={imgu}
+            alt=""
+          />
+          {/* <QRCode
+            fgColor={qr}
+            bgColor={bg}
+            style={{ height: height, width: width }}
+            value={value}
+          /> */}
         </div>
       </div>
     </div>
